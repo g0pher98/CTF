e33a8eb586dae5d28353bcfda972afcf6948b20e
g0pher-git <g0pher@tistory.com>
2019-08-01 03:37:57 +0900

    clear

--- 2018/HeXA/Reversing_Challenge1.py
+++
@@ -1,6 +0,0 @@
-s = chr(95)+chr(85)+chr(88)+chr(94)+chr(66)+chr(87)+chr(80)+chr(90)
-r = ""
-for i in s:
-    r += chr(ord(i)^int('0x39',16))
-
-print(r)
--- 2018/HeXA/Very_Safe_Site(100).py
+++
@@ -1,8 +0,0 @@
-import requests
-
-URL = "http://play.hexa.pro/problem/99f02f561cce563e851d22c4fb85e93a/"
-HEADERS = {"User-Agent" : "JOMUJE", "Referer":"jandimuje.ac.kr" }
-res = requests.get(URL, headers = HEADERS)
-print(res.text)
-
-#flag{N0W_y0u_caN_mak3_S3V3ral_T00ls!}
